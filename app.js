5b5e6127542ecc16b062ed1a4c9f6f7076d0edc2
Artem Bykov <mokijoy@gmail.com>
2017-11-02 16:58:11 +0300

    Add roll check, reason the turn change, minor changes

--- app.js
+++ app.js
@@ -15,28 +15,34 @@ init()
 
 document.querySelector('.btn-roll').addEventListener('click', function () {
     if (gamePlaying) {
-
+        confirmScores()
         var diceDOM = document.querySelector('.dice')
         var dice2DOM = document.querySelector('.dice-2')
+
         var dice = Math.floor(Math.random() * 6) + 1
         var dice2 = Math.floor(Math.random() * 6) + 1
-        //                var dice = 6
+
         dice2DOM.style.display = 'block'
         dice2DOM.src = 'dice-' + dice2 + '.png'
         diceDOM.style.display = 'block'
         diceDOM.src = 'dice-' + dice + '.png'
 
         if (dice !== 1 && dice2 !== 1) {
-            if (dice === 6) {
+            if (dice === 6 || dice2 === 6) {
                 sixRow++;
                 roundScore += dice + dice2
                 document.querySelector('#current-' + activePlayer).textContent = roundScore
+
                 if (sixRow === 2) {
                     sixRow = 0
                     scores[activePlayer] = 0
+                    document.querySelector('.reason-' + activePlayer).innerHTML = 'Dices are 6!'
+
                     document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer]
                     nextPlayer()
-                    return
+                    document.querySelector('.reason-' + activePlayer).innerHTML = ''
+
+                    return;
                 }
             } else {
                 sixRow = 0
@@ -47,7 +53,10 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
             //            roundScore += dice
             //            document.querySelector('#current-' + activePlayer).textContent = roundScore
         } else {
+            document.querySelector('.reason-' + activePlayer).innerHTML = 'Dice is 1!'
             nextPlayer();
+            document.querySelector('.reason-' + activePlayer).innerHTML = ''
+
         }
 
     }
@@ -99,9 +108,9 @@ function init() {
 
     scores = [0, 0];
     activePlayer = 0;
-    roundScore = 0
-    sixRow = 0
-    gamePlaying = true
+    roundScore = 0;
+    sixRow = 0;
+    gamePlaying = true;
 
     document.querySelector('.dice').style.display = 'none'
     document.querySelector('.dice-2').style.display = 'none'
@@ -130,11 +139,24 @@ document.querySelector('.btn-new').addEventListener('click', init)
 
 function confirmScores() {
     var inputValue = document.querySelector('#inputForScores').value
-    scoresForWin = inputValue
-    
+
+    if( inputValue == ''){
+        scoresForWin = 100;
+    }else {
+        scoresForWin = inputValue
+
+    }
+
     document.querySelector('#inputForScores').value = ''
     document.querySelector('#scoreButton').style.display = 'none'
     document.querySelector('#inputForScores').style.display = 'none'
 }
 
 document.querySelector('#scoreButton').addEventListener('click', confirmScores)
+
+
+/*
+* Отображать
+*
+*
+* */
